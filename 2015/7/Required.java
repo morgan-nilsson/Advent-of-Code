ff96f1aa46e2952303b8e0f2052067751e0ec0cb
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-12-01 11:55:07 -0700

    commit

--- 2015/7/Required.java
+++ 2015/7/Required.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
+import java.util.Vector;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Required {
+
+    static int ALPHA_LENGTH = 26;
+    static String[] binStrings = new String[ALPHA_LENGTH];
     public static void main(String[] args) {
 
-        int ALPHA_LENGTH = 26;
-
-        String[] binStrings = new String[ALPHA_LENGTH];
 
         if(args[0] == null){
             System.err.println("USAGE: java Required.java data.dat");
             return;
         }
 
-        Scanner input;
+        Scanner input = null;
         try {
             File file = new File(args[0]);
             input = new Scanner(file);
         } catch(FileNotFoundException e){
             System.err.println("Could not find the file with the given path");
+            input.close();
             return;
         } catch (Exception e) {
             System.err.println("Could not open the file at the given path");
@@ -27,13 +29,59 @@ public class Required {
         }
 
         while(input.hasNextLine()){
-            parse(input.nextLine());
+                try {
+                    parse(input.nextLine());
+                    printBinDump();
+                } catch (Exception e) {
+                    System.err.println("Error in the format of commands");
+                    input.close();
+                    e.printStackTrace();
+                    return;
+                }
         }
         input.close();
     }
 
     public static void parse(String line){
-        
+       Scanner input_line = new Scanner(line);
+        Vector<String> commands = new Vector<String>(); 
+        int cmdNum = 0;
+        char result = (char)-1;
+        char in1;
+        char in2;
+        String command;
+        int value;
+        boolean found = false;
+        while(input_line.hasNext()){
+            commands.add(input_line.next());
+            cmdNum++;
+        }
+        for(int i = 0; i < commands.size(); i++){
+            if(commands.get(i).compareTo("->") == 0){
+                result = commands.get(i + 1).charAt(0);
+            }
+        }
+        if(result == (char)-1){
+            System.err.println("Bad instruction set");
+            throw new java.lang.IllegalArgumentException(); 
+        }
+        for(int i = 0; i < commands.size(); i++){
+            String cmd = commands.get(i);
+            if(cmd.compareTo("AND") == 0){
+                binStrings[result - 97] = bitwiseAND(commands.get(i - 1).charAt(0), commands.get(i + 1).charAt(0));
+            } else if(cmd.compareTo("OR") == 0){
+                binStrings[result - 97] = bitwiserOR(commands.get(i - 1).charAt(0), commands.get(i + 1).charAt(0));
+            } else if(cmd.compareTo("NOT") == 0){
+                bitwiseNOT(commands.get(i + 1).charAt(0));
+            } else if(cmd.compareTo("RSHIFT") == 0){
+                bitRShift(commands.get(i - 1).charAt(0), Integer.parseInt(commands.get(i + 1)));
+            } else if(cmd.compareTo("LSHIFT") == 0){
+                bitLSHIFT(commands.get(i - 1).charAt(0), Integer.parseInt(commands.get(i + 1)));
+            }
+        }
+        if(!found){
+            setValue(commands.get(2).charAt(0), toBinary(Integer.parseInt(commands.get(0))));
+        }
     }
 
     public static void setValue(char c, String bin){
@@ -49,36 +97,60 @@ public class Required {
     }
 
     public static void printBinDump(){
+        int ALPHA_LENGTH = 26;
         for(int i = 0; i < ALPHA_LENGTH - 1; i++){
             if(binStrings[i] != null){
                 System.out.printf("%c : %d", i + 97, fromBinary(binStrings[i]));
             }
         }
+        System.out.println();
     }
 
     public static String bitwiseAND(char c, char d){
-        String anded;
-        for(int i = 0; i < binStrings[c - 97]; i++){
-            anded.concat(binStrings[c - 97] & binStrings[c - 97]);
+        String anded = "";
+        for(int i = 0; i < binStrings[c - 97].length(); i++){
+            anded.concat(Integer.toString(Integer.parseInt(binStrings[c - 97]) & Integer.parseInt(binStrings[c - 97])));
         }
         return anded;
     }
 
     public static String bitwiserOR(char c, char d){
-        String ored;
-        for(int i = 0; i < binStrings[c - 97]; i++){
-            ored.concat(binStrings[c - 97] | binStrings[c - 97]);
+        String ored = "";
+        for(int i = 0; i < binStrings[c - 97].length(); i++){
+            ored.concat(Integer.toString(Integer.parseInt(binStrings[c - 97]) | Integer.parseInt(binStrings[c - 97])));
         }
         return ored;
     }
 
-    public static String bitShift(char c, int shift){
-        StringBuffer shifted;
-        for(int i = binStrings[c - 97].length; i > 0; i--){
-            if(i + shift < binStrings[c - 97].length){
-                shifted.insert(i + shift, binStrings[i]);
+    public static String bitRShift(char c, int shift){
+        StringBuffer shifted = new StringBuffer("");
+        for(int i = binStrings[c - 97].length(); i > 0; i--){
+            if(i + shift < binStrings[c - 97].length()){
+                shifted.insert(i + shift, Integer.parseInt(binStrings[i]));
             }
         }
-        return shifted;
+        return shifted.toString();
+    }
+
+    public static String bitLSHIFT(char c, int shift){
+        StringBuffer shifted = new StringBuffer("");
+       for(int i = 0; i < binStrings[c - 97].length(); i++){
+            if(i - shift >= 0){
+                shifted.insert(i - shift, Integer.parseInt(binStrings[i]));
+            }
+       }
+       return shifted.toString();
+    }
+
+    public static String bitwiseNOT(char c){
+        StringBuffer noted = new StringBuffer("");
+        for(int i = 0; i < binStrings[c - 97].length(); i++){
+            if(binStrings[c - 97].charAt(i) == '0'){
+                noted.insert(i, '1');
+            } else{
+                noted.insert(i, '0');
+            }
+        }
+        return noted.toString();
     }
 }
