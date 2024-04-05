d62d247a3690008dd80efe289408452b2d7af7ca
morgan <morgan.nilsson7111@icloud.com>
2024-04-04 23:58:17 -0600

    commit

---
+++ 2023/16/Puzzle.java
@@ -0,0 +1,19 @@
+class Puzzle {
+
+	static final String[] puzzle = {
+		.|...\....
+		|.-.\.....
+		.....|-...
+		........|.
+		..........
+		.........\
+		..../.\\..
+		.-.-/..|..
+		.|....-|.\
+		..//.|....
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
