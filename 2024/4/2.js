311eee20dc9f10745ef17bbb793bbef5a2e24afc
mnilsson183 <morgan.nilsson7111@icloud.com>
2024-12-04 13:04:44 -0700

    4.2 code cleanup

--- 2024/4/2.js
+++ 2024/4/2.js
@@ -5,29 +5,21 @@ const lines = fs.readFileSync('data.dat').toString().split('\n')
 
 let xmas = 0
 
-console.log(lines)
-
 for (let i = 0; i < lines.length; i++) {
-	let init = xmas
 	for (let j = 0; j < lines[i].length; j++) {
 		if (lines[i][j] != 'A') continue
 		let left = false
 		let right = false
 
-		if (i == lines.length) continue
-		if (i == 0) continue
-		if (j == lines[i].length) continue
-		if (j == 0) continue
-		const leftSide = "" + (lines[i+1][j-1] || '0') + 'A' + (lines[i-1][j+1] || '0')
+		if (i == lines.length || j == lines[i].length) continue
+		if (i == 0 || j == 0) continue
+		const leftSide = "" + (lines[i+1][j-1]) + 'A' + (lines[i-1][j+1])
 		if (leftSide == "MAS" || leftSide == "SAM") left = true
 
-		const rightSide = "" + (lines[i+1][j+1] || '0') + 'A' + (lines[i-1][j-1] || '0')
+		const rightSide = "" + (lines[i+1][j+1]) + 'A' + (lines[i-1][j-1])
 		if (rightSide == "MAS" || rightSide == "SAM") right = true
 
 		if (right && left) xmas++
-
-		if (init != xmas) console.log(i, j, xmas - init)
-		init = xmas
 	}
 }
 
