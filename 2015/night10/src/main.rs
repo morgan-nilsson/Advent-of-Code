378a4ad11aa374f1c9051d737bc7f501232dda28
morgan-nilsson <morgan.nilsson0@proton.me>
2025-10-20 08:48:22 -0600

    night10

---
+++ 2015/night10/src/main.rs
@@ -0,0 +1,29 @@
+use std::fmt::Write;
+
+fn main() {
+    let input = "1113122113";
+    let iterations = 50;
+    let mut curr = input.to_string();
+
+    for _ in 0..iterations {
+        let mut next = String::new();
+        let mut chars = curr.chars();
+        let mut last = chars.next().unwrap();
+        let mut count = 1;
+
+        for ch in chars {
+            if ch == last {
+                count += 1;
+            } else {
+                write!(next, "{}{}", count, last).unwrap();
+                last = ch;
+                count = 1;
+            }
+        }
+
+        write!(next, "{}{}", count, last).unwrap();
+        curr = next;
+    }
+
+    println!("Length: {}", curr.len());
+}
